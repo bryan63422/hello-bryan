240d5365b58581671e50f819b5576039b4a73246
Bryan <bryan63422@gmail.com>
2019-08-28 12:37:51 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,20 @@
+/**
+ * Author: Boyang Hu
+ * Date: 2019/08/28
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Boyang Hu!\n");
+  printf("Computer Engineering");
+  
+  printf("Bryan!\n");
+  printf("Computer Science");
+
+  return 0;
+}
